0319efe9c9c4f7fb63d3013e5ed90f3b8272add1
harunkestendzic <136988209+harunkestendzic@users.noreply.github.com>
2024-10-05 16:52:50 +0200

    Add files via upload

---
+++ Movie management/bookMovies.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once("config_movie.php");
+
+$user_id=$_SESSION['id'];
+$movie_id=$_SESSION['id'];
+
+$nr_tickets=$_POST['nr_tickets'];
+$date=$_POST['date'];
+$time=$_POST['time'];
+
+$sql="INSERT INTO bookings(user_id,movie_id,nr_tickets,date,time)
+VALUES (:user_id,:movie_id,:nr_tickets,:date,:time)";
+
+$insertBooking=$conn->prepare($sql);
+$insertBooking->bindParam(":user_id",$user_id);
+$insertBooking->bindParam(":movie_id",$movie_id);
+$insertBooking->bindParam(":nr_tickets",$nr_tickets);
+$insertBooking->bindParam(":date",$date);
+$insertBooking->bindParam(":time",$time);
+
+$insertBooking->execute();
+?>
---
+++ Movie management/logoutMovies.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once('config_movie.php');
+session_destroy();
+header("Location:login_movies.php");
+
+?>
